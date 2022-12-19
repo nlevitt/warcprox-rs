45caa21a12c9b81478171dae01984494ea4d8d81
Noah Levitt <nlevitt@benchling.com>
2022-12-18 22:44:51 -0800

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
-use futures::FutureExt;
-use futures::future::Map;
 use futures::{Stream, StreamExt};
-use http_body::Data;
 use hudsucker::{
     async_trait::async_trait,
     certificate_authority::RcgenAuthority,
-    hyper::body::HttpBody,
     hyper::{Body, Request, Response},
     rustls,
     tokio_tungstenite::tungstenite::Message,
@@ -19,7 +15,6 @@ use rcgen::{
 use std::net::SocketAddr;
 use std::pin::Pin;
 use std::task::{Context, Poll};
-use tokio::io::AsyncWriteExt;
 use tracing::*; // for mpsc::Receiver
 
 async fn shutdown_signal() {
@@ -28,23 +23,9 @@ async fn shutdown_signal() {
         .expect("Failed to install CTRL+C signal handler");
 }
 
-// async fn process_body<'a>(mut body: Body) -> Result<Body, Error> {
-//     while let Some(buf) = body.data().await {
-//         info!("{:?}", buf);
-//     }
-//     Ok(body)
-// }
+struct IoStream<T: Stream<Item = Result<Bytes, Error>> + Unpin>(T);
 
-// fn process_response(mut res: Response<Body>) -> Result<Response<Body>, Error> {
-//     let (mut parts, body) = res.into_parts();
-//     // let body = process_body(body);
-//     body.poll_next()
-//     Ok(Response::from_parts(parts, body))
-// }
-
-struct IoStream<T: Stream<Item = Result<Bytes, hyper::Error>> + Unpin>(T);
-
-impl<T: Stream<Item = Result<Bytes, hyper::Error>> + Unpin> Stream for IoStream<T> {
+impl<T: Stream<Item = Result<Bytes, Error>> + Unpin> Stream for IoStream<T> {
     type Item = Result<Bytes, std::io::Error>;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
@@ -60,10 +41,10 @@ impl<T: Stream<Item = Result<Bytes, hyper::Error>> + Unpin> Stream for IoStream<
 }
 
 #[derive(Clone)]
-struct LogHandler;
+struct WarcProxyHandler;
 
 #[async_trait]
-impl HttpHandler for LogHandler {
+impl HttpHandler for WarcProxyHandler {
     async fn handle_request(
         &mut self,
         _ctx: &HttpContext,
@@ -75,48 +56,18 @@ impl HttpHandler for LogHandler {
 
     async fn handle_response(&mut self, _ctx: &HttpContext, res: Response<Body>) -> Response<Body> {
         info!("{:?}", res);
-        // let res = process_response(res).unwrap();
-        let (parts, mut body) = res.into_parts();
-
-        // while let Some(next) = res.data().await {
-        //     let chunk = next.unwrap();
-        //     tokio::io::stdout().write_all(&chunk).await.unwrap();
-        // }
+        let (parts, body) = res.into_parts();
 
         let body = Body::wrap_stream(IoStream(body).map(|buf| {
             info!("{:?}", buf);
             buf
         }));
-        // Self::Body(body) => Box::new(StreamReader::new(IoStream(body))),
-        // let data: Map<
-        //     Data<Body>,
-        //     fn(Option<Result<Bytes, Error>>) -> Option<Result<Bytes, Error>>,
-        // > = body.data().map(|buf| {
-        //     info!("{:?}", buf.unwrap().unwrap());
-        //     buf
-        // });
-        // let body = Body::wrap_stream(data);
-        // let body = Body::from(data);
-        // Body::from();
-        // data.then(|x| {
-        //     x.unwrap().unwrap();
-        // });
-
-        // let body = body.then(|buf| async move { x + 3 });
-        // let s: dyn Stream = body.into();
-
-        // let body = body.then(|buf| {
-        //     info!(buf);
-        //     Ok(buf)
-        // });
-        // Response::from_parts(parts, body.into())
         Response::from_parts(parts, body)
-        // Response::new(Body::from('hello'))
     }
 }
 
 #[async_trait]
-impl WebSocketHandler for LogHandler {
+impl WebSocketHandler for WarcProxyHandler {
     async fn handle_message(&mut self, _ctx: &WebSocketContext, msg: Message) -> Option<Message> {
         info!("{:?}", msg);
         Some(msg)
@@ -152,8 +103,8 @@ async fn main() {
         .with_addr(addr)
         .with_rustls_client()
         .with_ca(ca)
-        .with_http_handler(LogHandler)
-        .with_websocket_handler(LogHandler)
+        .with_http_handler(WarcProxyHandler)
+        .with_websocket_handler(WarcProxyHandler)
         .build();
 
     info!("proxy listening at {}", addr);
