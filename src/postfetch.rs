6f8982447c9647c0bff1b5e3fbdcf881ce329e85
Noah Levitt <nlevitt@benchling.com>
2023-03-26 00:04:27 -0700

    refactor warcproxy, postfetch

--- src/postfetch.rs
+++ src/postfetch.rs
@@ -1,169 +1,13 @@
 use crate::recorded_url::RecordedUrl;
+use crate::rolling_warc_writer::RollingWarcWriter;
+use futures::channel::mpsc;
+use futures::StreamExt as _;
 use std::borrow::Cow;
-
-use aho_corasick::AhoCorasick;
-use chrono::Utc;
-use futures::channel::mpsc::Receiver;
-use futures::StreamExt;
-use gethostname::gethostname;
-use rand::distributions::uniform::Uniform;
-use rand::distributions::Distribution;
-use rand::{thread_rng, Rng};
-use std::fs::{create_dir, File};
-use std::io::{Error, Read};
-use std::path::Path;
+use std::future::Future;
+use std::path::PathBuf;
 use tempfile::SpooledTempFile;
 use tracing::info;
-use warcio::{WarcRecord, WarcRecordInfo, WarcRecordWrite, WarcWriter};
-
-struct RollingWarcWriter<'a> {
-    dir: &'a Path,
-    filename_template: String,
-    gzip: bool,
-    rollover_size: u64,
-    current_warc_filename: Option<Vec<u8>>,
-    inner: Option<WarcWriter<File>>,
-    n: u64,
-    random_token: String,
-    port: u16,
-}
-
-const TEMPLATE_PARAM_KEYS: [&str; 7] = [
-    "{timestamp14}",
-    "{timestamp17}",
-    "{serialno}",
-    "{randomtoken}",
-    "{maybe_dot_gz}",
-    "{hostname}",
-    // "{shorthostname}",
-    "{port}",
-];
-
-struct LowercaseAlphanumeric;
-const LOWERCASE_ALPHANUMERIC_BYTES: &[u8] = b"abcdefghijklmnopqrstuvwxyz0123456789";
-
-impl<'a> RollingWarcWriter<'a> {
-    fn new(
-        dir: &'a Path,
-        filename_template: String,
-        gzip: bool,
-        rollover_size: u64,
-        port: u16,
-    ) -> Self {
-        let random_token: String = (0..8)
-            .map(|_| thread_rng().sample(LowercaseAlphanumeric))
-            .collect();
-        Self {
-            dir,
-            filename_template,
-            gzip,
-            rollover_size,
-            current_warc_filename: None,
-            inner: None,
-            n: 0,
-            random_token,
-            port,
-        }
-    }
-
-    fn build_filename(&self) -> String {
-        // todo: cache these in RollingWarcWriter?
-        let template_param_finder: AhoCorasick = AhoCorasick::new(TEMPLATE_PARAM_KEYS);
-
-        impl Distribution<char> for LowercaseAlphanumeric {
-            fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> char {
-                let byte_picker: Uniform<usize> =
-                    Uniform::from(0..LOWERCASE_ALPHANUMERIC_BYTES.len());
-                LOWERCASE_ALPHANUMERIC_BYTES[byte_picker.sample(rng)] as char
-            }
-        }
-
-        let mut new_filename_buf = String::new();
-        template_param_finder.replace_all_with(
-            &self.filename_template,
-            &mut new_filename_buf,
-            |_, match_, output_buf| {
-                if match_ == "{timestamp17}" {
-                    // let t17 = Utc::now().format("%Y%m%d%H%M%S%.3f").to_string(); // extra dot
-                    let now = Utc::now();
-                    let mut t17 = now.format("%Y%m%d%H%M%S").to_string();
-                    t17.push_str(&format!("{:03}", now.timestamp_subsec_millis()));
-                    output_buf.push_str(&t17);
-                } else if match_ == "{timestamp14}" {
-                    let now = Utc::now();
-                    let t14 = now.format("%Y%m%d%H%M%S").to_string();
-                    output_buf.push_str(&t14);
-                } else if match_ == "{hostname}" {
-                    output_buf.push_str(
-                        &gethostname()
-                            .into_string()
-                            .unwrap_or(String::from("unknown")),
-                    );
-                } else if match_ == "{port}" {
-                    output_buf.push_str(&self.port.to_string());
-                } else if match_ == "{serialno}" {
-                    output_buf.push_str(&format!("{:05}", self.n));
-                } else if match_ == "{randomtoken}" {
-                    output_buf.push_str(&self.random_token);
-                } else if match_ == "{maybe_dot_gz}" {
-                    if self.gzip {
-                        output_buf.push_str(".gz");
-                    }
-                }
-                true
-            },
-        );
-        new_filename_buf
-    }
-
-    /// Returns Ok((warc_path, offset))
-    fn write_records<R: Read>(
-        &mut self,
-        records: Vec<WarcRecord<R>>,
-    ) -> Result<WarcRecordInfo, Error> {
-        if self.inner.is_some() && self.inner.as_mut().unwrap().tell() > self.rollover_size {
-            // It's time to roll over
-            self.inner = None;
-        }
-
-        if self.inner.is_none() {
-            // Either we just started or we just rolled over, so open new warc
-            if !self.dir.exists() {
-                create_dir(self.dir)?;
-            }
-
-            let warc_filename = self.build_filename();
-
-            let mut p = self.dir.to_path_buf();
-            p.push(&warc_filename);
-            let f = File::create(&p)?;
-            let w = WarcWriter::new(f, self.gzip);
-
-            self.inner = Some(w);
-            self.current_warc_filename = Some(Vec::from(warc_filename.as_bytes()));
-            self.n += 1;
-        }
-
-        // Only now do we know the `WARC-Filename` 🫠 What do we do?
-        // - Keep the WarcRecordBuilder around until now?
-        // - Pass filename as argument to `write_record`? (Going with this for now)
-        // - Make WarcWriter aware of it?
-        // - Set it on the WarcRecord directly here?
-
-        let warc_writer = self.inner.as_mut().unwrap();
-        let mut first_record_info = None;
-        for record in records {
-            let warc_record_info =
-                warc_writer.write_record(record, (&self.current_warc_filename).as_ref())?;
-
-            if first_record_info.is_none() {
-                first_record_info = Some(warc_record_info);
-            }
-        }
-
-        Ok(first_record_info.unwrap())
-    }
-}
+use warcio::{WarcRecord, WarcRecordInfo};
 
 fn log(warc_record_info: WarcRecordInfo) {
     let status_str = match &warc_record_info.http_metadata.status {
@@ -206,27 +50,41 @@ fn log(warc_record_info: WarcRecordInfo) {
     );
 }
 
-pub(crate) fn spawn_postfetch(
-    mut rx: Receiver<RecordedUrl>,
-    gzip: bool,
-    warc_filename_template: String,
-    port: u16,
-) {
-    tokio::spawn(async move {
-        let mut warc_writer = RollingWarcWriter::new(
-            "./warcs".as_ref(),
-            warc_filename_template,
-            gzip,
-            1_000_000_000,
-            port,
-        );
-        while let Some(recorded_url) = rx.next().await {
+pub(crate) struct Postfetch {
+    warc_writer: RollingWarcWriter,
+    recorded_url_rx: mpsc::Receiver<RecordedUrl>,
+}
+
+impl Postfetch {
+    pub fn new(
+        recorded_url_rx: mpsc::Receiver<RecordedUrl>,
+        warc_filename_template: String,
+        gzip: bool,
+        port: u16,
+    ) -> Self {
+        Postfetch {
+            recorded_url_rx,
+            warc_writer: RollingWarcWriter::new(
+                PathBuf::from("./warcs"), // todo: make this a command line option
+                warc_filename_template,
+                gzip,
+                1_000_000_000, // todo: make this a command line option
+                port,
+            ),
+        }
+    }
+
+    pub async fn start<F: Future<Output = ()> + Send + 'static>(
+        mut self,
+        shutdown_signal: F,
+    ) -> Result<(), std::io::Error> {
+        while let Some(recorded_url) = self.recorded_url_rx.next().await {
             let records = Vec::<WarcRecord<SpooledTempFile>>::from(recorded_url);
-            let warc_record_info = warc_writer.write_records(records)?;
+            let warc_record_info = self.warc_writer.write_records(records)?;
             log(warc_record_info);
         }
-        Ok::<(), Error>(())
-    });
+        Ok(())
+    }
 }
 
 // todo:
--- src/warcproxy.rs
+++ src/warcproxy.rs
@@ -1,9 +1,10 @@
 use crate::ca;
-use crate::postfetch::spawn_postfetch;
+use crate::postfetch::Postfetch;
 use crate::proxy::ProxyTransactionHandler;
 use crate::proxy_client::proxy_client;
 use crate::recorded_url::RecordedUrl;
 use futures::channel::mpsc;
+use futures::try_join;
 use hudsucker::certificate_authority::RcgenAuthority;
 use hudsucker::hyper::client::HttpConnector;
 use hudsucker::{NoopHandler, Proxy};
@@ -11,15 +12,18 @@ use hyper_rustls::HttpsConnector;
 use std::error::Error;
 use std::future::Future;
 use std::net::{SocketAddr, TcpListener};
+use tokio::task::JoinHandle;
 
 pub struct WarcProxy {
     pub addr: SocketAddr,
-    proxy:
+    proxy: Option<
         Proxy<HttpsConnector<HttpConnector>, RcgenAuthority, ProxyTransactionHandler, NoopHandler>,
-    recorded_url_rx: mpsc::Receiver<RecordedUrl>,
-    gzip: bool,
-    warc_filename_template: String,
-    port: u16,
+    >,
+    postfetch: Option<Postfetch>,
+    // recorded_url_rx: mpsc::Receiver<RecordedUrl>,
+    // gzip: bool,
+    // warc_filename_template: String,
+    // port: u16,
 }
 
 impl WarcProxy {
@@ -35,38 +39,96 @@ impl WarcProxy {
         let port = tcp_listener.local_addr()?.port();
         Ok(Self {
             addr: tcp_listener.local_addr()?,
-            proxy: Proxy::builder()
-                .with_listener(tcp_listener)
-                .with_client(proxy_client())
-                .with_ca(ca)
-                .with_http_handler(ProxyTransactionHandler::new(recorded_url_tx))
-                .build(),
-            recorded_url_rx,
-            gzip,
-            warc_filename_template,
-            port,
+            proxy: Some(
+                Proxy::builder()
+                    .with_listener(tcp_listener)
+                    .with_client(proxy_client())
+                    .with_ca(ca)
+                    .with_http_handler(ProxyTransactionHandler::new(recorded_url_tx))
+                    .build(),
+            ),
+            postfetch: Some(Postfetch::new(
+                recorded_url_rx,
+                warc_filename_template,
+                gzip,
+                port,
+            )),
         })
     }
 
-    pub async fn run_until_shutdown<F: Future<Output = ()>>(
+    /// Returns tuple (proxy_join_handle, postfetch_join_handle). Your code doesn't necessarily
+    /// have to do anything with those though. They are useful for tests.
+    pub fn spawn<F: Future<Output = ()> + Send + 'static>(
+        mut self,
+        shutdown_signal: F,
+    ) -> (
+        JoinHandle<Result<(), hudsucker::Error>>, // proxy join handle
+        JoinHandle<Result<(), std::io::Error>>,   // postfetch join handle
+    ) {
+        (
+            tokio::spawn(self.proxy.take().unwrap().start(shutdown_signal)),
+            tokio::spawn(self.postfetch.take().unwrap().start(shutdown_signal)),
+        )
+    }
+
+    /// Spawns proxy and postfetch and waits for them to finish.
+    ///
+    /// Can return an error from the futures level, or from the proxy, or from postfetch.
+    ///
+    /// If both postfetch and proxy an error, the proxy error is returned and the postfetch error is
+    /// swallowed. Returning both would make the function signature ugly. todo: reevaluate this
+    /// decision
+    pub async fn run_until_shutdown<F: Future<Output = ()> + Send + 'static>(
         self,
         shutdown_signal: F,
-    ) -> Result<(), hudsucker::Error> {
-        spawn_postfetch(
-            self.recorded_url_rx,
-            self.gzip,
-            self.warc_filename_template,
-            self.port,
-        );
-        self.proxy.start(shutdown_signal).await
+    ) -> Result<(), Box<dyn Error>> {
+        let (proxy_join_handle, postfetch_join_handle) = self.spawn(shutdown_signal);
+        let (proxy_join_result, postfetch_join_result) =
+            try_join!(proxy_join_handle, postfetch_join_handle)?;
+        if proxy_join_result.is_err() {
+            proxy_join_result.map_err(|e| e.into())
+        } else if postfetch_join_result.is_err() {
+            postfetch_join_result.map_err(|e| e.into())
+        } else {
+            Ok(())
+        }
     }
 }
 
 // todo:
 //
-// #[cfg(test)]
-// mod tests {
-//     fn test_new_does_not_start_anything () {}
-//     fn test_run_until_shutdown_starts_proxy_and_postfetch() {}
-//     fn test_shutdown_signal_stops_proxy_and_postfetch() {}
-// }
+#[cfg(test)]
+mod tests {
+    use crate::WarcProxy;
+    use futures::channel::oneshot;
+    use futures::FutureExt;
+    use std::error::Error;
+    use tempfile::NamedTempFile;
+
+    // fn test_new_does_not_start_anything () {}
+    // fn test_run_until_shutdown_starts_proxy_and_postfetch() {}
+
+    #[test_log::test(tokio::test)]
+    async fn test_shutdown_signal_stops_proxy_and_postfetch() -> Result<(), Box<dyn Error>> {
+        let ca_cert = NamedTempFile::new()?.into_temp_path();
+        let ca_cert_path = String::from(ca_cert.to_str().unwrap());
+        ca_cert.close()?;
+        let warcproxy = WarcProxy::new(
+            "localhost:0",
+            true,
+            &ca_cert_path,
+            String::from("warcprox-test-{serialno}.warc"),
+        )?;
+
+        let (shutdown_oneshot_tx, shutdown_oneshot_rx) = oneshot::channel::<()>();
+        let (proxy_join_handle, postfetch_join_handle) =
+            warcproxy.spawn(shutdown_oneshot_rx.map(|_| ()));
+        shutdown_oneshot_tx.send(()).unwrap();
+
+        // todo: wait with timeout? poll is_finished() with timeout?
+        proxy_join_handle.await??;
+        postfetch_join_handle.await??;
+
+        Ok(())
+    }
+}
